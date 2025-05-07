a001af7d0d1e11de978f3b5ecfb063d8547a480c
LiliaSolis <liliadata@gmail.com>
2025-05-06 21:25:23 -0700

    Project 7 added new file

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import data
-import data
 import helpers
 from helpers import is_url_reachable
 
@@ -12,49 +11,41 @@ class TestUrbanRoutes:
         else:
             return print("Cannot connect to Urban Routes. Check the server is on and still running")
 
+    def test_set_route(self):  # Add in S8
+        print("function created for set route")
+        pass  # Placeholder for future implementation
 
-def test_set_route(self):  # Add in S8
-    print("function created for set route")
-    pass  # Placeholder for future implementation
+    def test_select_plan(self):  # Add in S8
+        print()
+        pass  # Placeholder for future implementation
 
+    def test_fill_phone_number(self):  # Add in S8
+        print()
+        pass  # Placeholder for future implementation
 
-def test_select_plan(self):  # Add in S8
-    print()
-    pass  # Placeholder for future implementation
+    def test_fill_card(self):  # Add in S8
+        print()
+        pass  # Placeholder for future implementation
 
+    def test_comment_for_driver(self):  # Add in S8
+        print()
+        pass  # Placeholder for future implementation
 
-def test_fill_phone_number(self):  # Add in S8
-    print()
-    pass  # Placeholder for future implementation
+    def test_order_blanket_and_handkerchiefs(self):  # Add in S8
+        print()
+        pass  # Placeholder for future implementation
 
-
-def test_fill_card(self):  # Add in S8
-    print()
-    pass  # Placeholder for future implementation
-
-
-def test_comment_for_driver(self):  # Add in S8
-    print()
-    pass  # Placeholder for future implementation
-
-
-def test_order_blanket_and_handkerchiefs(self):  # Add in S8
-    print()
-    pass  # Placeholder for future implementation
-
-
-def test_order_2_ice_creams(self):  # Add in S8
-    for order in range(2)
+    def test_order_2_ice_creams(self):  # Add in S8
+        for order in range(2)
         print(order)
-    pass  # Placeholder for future implementation
+        pass  # Placeholder for future implementation
+
+    def test_car_search_model_appears(self):  # Add in S8
+        print()
+        pass  # Placeholder for future implementation
 
 
-def test_car_search_model_appears(self):  # Add in S8
-    print()
-    pass  # Placeholder for future implementation
-
-
-if check.hostname == "x":
-    return True
-else:
-    return False
+        if check.hostname == "x":
+            return True
+        else:
+            return False
