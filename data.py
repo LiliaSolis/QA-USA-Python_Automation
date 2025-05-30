b721665bd354db822ad063acee6d3ebf99a785a8
LiliaSolis <liliadata@gmail.com>
2025-05-29 22:59:35 -0700

    Merge remote-tracking branch 'origin/main' into main

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 # File with constants
-URBAN_ROUTES_URL = "https://cnt-590ac9ba-edbc-493c-bcb8-2a10fbd0e9f2.containerhub.tripleten-services.com/"
+URBAN_ROUTES_URL = "https://cnt-491f24c0-d2c3-49cc-99dc-064455e5d78f.containerhub.tripleten-services.com/"
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = '+1 123 123 12 12'
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from time import sleep
-
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 import data
 import helpers
@@ -85,25 +85,6 @@ class TestUrbanRoutes:
         self.driver.get(data.URBAN_ROUTES_URL)
         routes_page = UrbanRoutesPage(self.driver)
         routes_page.set_route(data.ADDRESS_FROM, data.ADDRESS_TO)
-        routes_page.click_supportive_plan()
-        routes_page.click_phone_number_button()
-        routes_page.set_phone_number(data.PHONE_NUMBER)
-        routes_page.click_next_button()
-        routes_page.set_phone_number_code(helpers.retrieve_phone_code(self.driver))
-        routes_page.click_confirm_button()
-        routes_page.click_payment_method_button()
-        routes_page.click_add_card_button()
-        routes_page.set_card_number(data.CARD_NUMBER)
-        routes_page.set_code_field(data.CARD_CODE)
-        routes_page.click_link_button()
-        routes_page.set_message_to_the_driver(data.MESSAGE_FOR_DRIVER)
-        routes_page.get_blanket_and_handkerchiefs()
-        routes_page.click_order_2_ice_creams()
-        sleep(5)
-        for order in range(2):
-            sleep(5)
-            print(order)
-        sleep(5)
         routes_page.click_enter_the_number_and_order()
 
     @classmethod
